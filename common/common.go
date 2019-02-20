b1cbb8cda251052d38d1879752bc95aff8d9b7ce
tylerw1369 <39392335+tylerw1369@users.noreply.github.com>
2019-02-19 21:05:53 -0700

    Update common.go

--- common/common.go
+++ common/common.go
@@ -3,43 +3,17 @@ package common
 import (
 	"sync"
 
-	"github.com/iotaledger/giota"
+	"github.com/tylerw1369/iotago"
 )
 
 const (
-	DiverDriverVersion = "0.2.0"
+	DiverDriverVersion = "0.0.1"
 )
 
-type PowFuncDefinition func(p *DiverClient, trytes giota.Trytes, minWeightMagnitude int) (result giota.Trytes, Error error)
-type GetPowInfoDefinition func(p *DiverClient) (ServerVersion string, PowType string, PowVersion string, Error error)
+type PowFuncDefinition func(trytes giota.Trytes, minWeightMagnitude int) (result giota.Trytes, Error error)
+type GetPowInfoDefinition func(ServerVersion string, PowType string, PowVersion string, Error error)
 
 type ClientAPI struct {
 	PowFuncDefinition    PowFuncDefinition
 	GetPowInfoDefinition GetPowInfoDefinition
-}
-
-// DiverClient is the client that connects to the diverDriver
-type DiverClient struct {
-	PowClientImplementation *ClientAPI
-	DiverDriverPath         string // Path to the diverDriver Unix socket
-	WriteTimeOutMs          int64  // Timeout in ms to write to the Unix socket
-	ReadTimeOutMs           int    // Timeout in ms to read the Unix socket
-	RequestId               byte
-	RequestIdLock           sync.Mutex
-}
-
-func (p *DiverClient) PowFunc(trytes giota.Trytes, minWeightMagnitude int) (result giota.Trytes, Error error) {
-	return p.PowClientImplementation.PowFuncDefinition(p, trytes, minWeightMagnitude)
-}
-
-func (p *DiverClient) GetPowFuncDefinition() PowFuncDefinition {
-	return p.PowClientImplementation.PowFuncDefinition
-}
-
-func (p *DiverClient) GetPowInfo() (ServerVersion string, PowType string, PowVersion string, Error error) {
-	return p.PowClientImplementation.GetPowInfoDefinition(p)
-}
-
-func (p *DiverClient) GetPowInfoFuncDefinition() PowFuncDefinition {
-	return p.PowClientImplementation.PowFuncDefinition
 }
